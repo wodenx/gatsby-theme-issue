8b4dc3506a5bc00a118fadd9766a000f8b6ea8eb
Chris Oden <coden@its.jnj.com>
2019-05-14 21:33:45 -0400

    Add theme dependency

--- packages/site/gatsby-config.js
+++ packages/site/gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  __experimentalThemes: ['gatsby-theme-bodiless'],
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
